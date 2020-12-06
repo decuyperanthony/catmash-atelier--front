7128245bf3acc0ba409a46e9fb2bdf9d8499a6cf
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-12-06 22:30:13 +0100

    Create two components RandomMatch & Ranking in App.js

---
+++ src/App/RandomMatch/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './styles.scss';
+
+const RandomMatch = () => {
+    console.log('random');
+    return(
+        <div>RandDom match</div>
+    )
+};
+
+export default RandomMatch;
---
+++ src/App/Ranking/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './styles.scss';
+
+const Ranking = () => {
+    console.log('Ranking');
+    return(
+        <div>Ranking</div>
+    )
+};
+
+export default Ranking;
