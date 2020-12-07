11260280f3312b7c926e67b601b6d54e45d3a5ed
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-12-07 02:23:40 +0100

    update Ranking.js component =>
    - getRank done

--- src/App/Ranking/index.js
+++ src/App/Ranking/index.js
@@ -1,10 +1,34 @@
 import React from "react";
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+import getRank from '../../utils/getRank';
 import './styles.scss';
 
 const Ranking = () => {
-    console.log('Ranking');
+    const { rank } = useSelector((state) => state);
+    React.useEffect(() => {
+        getRank();
+    }, []);
+    let rankJSX = (<div>Loading...</div>);
+    if (rank.length > 0) {
+        rankJSX = rank.map((r) => {
+            return(
+                <div key={r.cat_id}>
+                    <div>
+                        {r.cat.name}
+                         : {r.votecount}
+                         <img style={{height: '10em', width: 'auto'}} alt="cat" src={r.cat.image_path}/>
+                    </div>
+                </div>
+            )
+        });
+    }
     return(
-        <div>Ranking</div>
+        <>
+          <Link to="/">match</Link>
+          <div>{rankJSX}</div>
+        </>
     )
 };
 
