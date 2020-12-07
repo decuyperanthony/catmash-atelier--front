5cc6b6895f7f7dcbd81a0a16821fecb9c0d2bb79
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-12-07 02:22:56 +0100

    update RandonMatch : =>
    - onClick method => req on api :+1:
    - get Random match

--- src/App/RandomMatch/index.js
+++ src/App/RandomMatch/index.js
@@ -1,10 +1,42 @@
 import React from "react";
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom'
+import getOneRandomMatch from '../../utils/getOneRandomMatch';
+import addVoteAndCat from '../../utils/addVoteAndCat';
+
 import './styles.scss';
 
 const RandomMatch = () => {
-    console.log('random');
+    const { match } = useSelector((state) => state);
+
+    React.useEffect(() => {
+        getOneRandomMatch();
+    }, []);
+
+    const handleVote = (name, imagePath) => {
+        const body = {
+            name,
+            image_path: imagePath
+        };
+        addVoteAndCat(body);
+
+    }
+
+    let matchJSX = (<div>Loading...</div>);
+    if (match.length > 0) {
+        matchJSX = match.map((m) => {
+            return(
+                <div onClick={() => handleVote(m.id, m.url)} key={m.id} className="cat-container">
+                    <img style={{height: '20em', width: 'auto'}} src={m.url} alt="cat"/>
+                </div>
+            )
+        });
+    }
     return(
-        <div>RandDom match</div>
+        <>
+          <div>{matchJSX}</div>
+          <Link to="/rank">classement</Link>
+        </>
     )
 };
 
